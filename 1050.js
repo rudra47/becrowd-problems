f2bb545e0b08e1e1bdaaba849b2f4561799da415
RudraSen <rudra.sen@technovista.com.bd>
2023-11-16 11:13:25 +0600

    condition added for 61

---
+++ 1050.js
@@ -0,0 +1,26 @@
+//DDD
+var input = `11`;
+var lines = input.split('\n');
+
+let number = Number(lines[0]);
+
+if (number === 61) {
+    console.log("Brasilia");
+}
+
+// Read an integer number that is the code number for phone dialing. Then, print the destination 
+// according to the following table:
+
+// If the input number isn’t found in the above table, the output must be:
+// DDD nao cadastrado
+// That means “DDD not found” in Portuguese language.
+
+// Input
+// The input consists in a unique integer number.
+
+// Output
+// Print the city name corresponding to the input DDD. Print DDD nao cadastrado if doesn't exist 
+// corresponding DDD to the typed number.
+
+// Input Sample	Output Sample
+// 11           Sao Paulo
