490aa88152f2db1a902b5ecef942bc93aeb534c4
RudraSen <rudra.sen@technovista.com.bd>
2023-11-19 12:18:18 +0600

    1052 Month problem solve

--- 1052.js
+++ 1052.js
@@ -4,7 +4,31 @@ var lines = input.split('\n');
 
 let number = Number(lines[0]);
 
-
+if (number === 1) {
+    console.log('January');
+}else if (number === 2) {
+    console.log('February');
+}else if (number === 3) {
+    console.log('March');
+}else if (number === 4) {
+    console.log('April');
+}else if (number === 5) {
+    console.log('May');
+}else if (number === 6) {
+    console.log('June');
+}else if (number === 7) {
+    console.log('July');
+}else if (number === 8) {
+    console.log('August');
+}else if (number === 9) {
+    console.log('September');
+}else if (number === 10) {
+    console.log('October');
+}else if (number === 11) {
+    console.log('November');
+}else if (number === 12) {
+    console.log('December');
+}
 
 
 // Read an integer number between 1 and 12, including. Corresponding to this number, you must print 
