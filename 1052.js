9e52f5ce65f6eefc93134558f1e3aa52e5700dbf
RudraSen <rudra.sen@technovista.com.bd>
2023-11-19 12:12:39 +0600

    catch the number from user

---
+++ 1052.js
@@ -0,0 +1,21 @@
+//MONTH
+var input = `4`;
+var lines = input.split('\n');
+
+let number = Number(lines[0]);
+
+
+
+
+// Read an integer number between 1 and 12, including. Corresponding to this number, you must print 
+// the month of the year, in english, with the first letter in uppercase.
+
+// Input
+// The input contains only an integer number.
+
+// Output
+// Print the name of the month according to the input number, with the first letter in uppercase.
+
+
+// Input Sample	Output Sample
+// 4            April
