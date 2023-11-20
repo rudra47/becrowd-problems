2987d4f864a0366adc40efb5e27c0332925470af
RudraSen <rudra.sen@technovista.com.bd>
2023-11-20 10:01:58 +0600

    even number problem ongoing

---
+++ 1059.js
@@ -0,0 +1,19 @@
+//EVEN NUMBER
+for (let index = 1; index <= 100; index++) {
+    
+}
+
+// Write a program that prints all even numbers between 1 and 100, including them if it is the case.
+
+// Input
+// In this extremely simple problem there is no input.
+
+// Output
+// Print all even numbers between 1 and 100, including them, one by row.
+
+// Input Sample	Output Sample
+//                 2
+//                 4
+//                 6
+//                 ...
+//                 100
