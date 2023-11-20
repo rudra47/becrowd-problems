cd0b2a75d055434ef52c6e92119583914fcaf394
RudraSen <rudra.sen@technovista.com.bd>
2023-11-20 10:13:41 +0600

    define problem as comment. define input fields

---
+++ 1060.js
@@ -0,0 +1,28 @@
+//MONTH
+var input = `7
+-5
+6
+-3.4
+4.6
+12`;
+var lines = input.split('\n');
+
+
+
+
+// Write a program that reads 6 numbers. These numbers will only be positive or negative (disregard null values). Print the total 
+// number of positive numbers.
+
+// Input
+// Six numbers, positive and/or negative.
+
+// Output
+// Print a message with the total number of positive numbers.
+
+// Input Sample	Output Sample
+// 7            4 valores positivos
+// -5
+// 6
+// -3.4
+// 4.6
+// 12
