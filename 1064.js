03da1d4661bc47c2f300c8f1bd969d31d7a56b30
RudraSen <rudra.sen@technovista.com.bd>
2023-11-22 15:13:13 +0600

    1064 no. problem file generated and solution ongoing

---
+++ 1064.js
@@ -0,0 +1,30 @@
+//POSITIVE AND AVERAGE
+var input = `7
+-5
+6
+-3.4
+4.6
+12`;
+var lines = input.split('\n');
+
+
+// Read 6 values that can be floating point numbers. After, print how many of them were positive. In the next line, print the 
+// average of all positive values typed, with one digit after the decimal point.
+
+// Input
+// The input consist in 6 numbers that can be integer or floating point values. At least one number will be positive.
+
+// Output
+// The first output value is the amount of positive numbers. The next line should show the average of the positive values ​typed.
+
+// Input Sample	
+// 7
+// -5
+// 6
+// -3.4
+// 4.6
+// 12
+
+// Output Sample
+// 4 valores positivos
+// 7.4
