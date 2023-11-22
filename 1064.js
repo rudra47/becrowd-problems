ebcc3e0ad76b659871c7dd4b307847dc648f447c
RudraSen <rudra.sen@technovista.com.bd>
2023-11-22 15:15:22 +0600

    log generated for output

--- 1064.js
+++ 1064.js
@@ -21,6 +21,8 @@ for (let index = 0; index < lines.length; index++) {
 
 let avg = sumOfPositiveNum / countOfPositiveNum;
 
+console.log(`${countOfPositiveNum} valores positivos
+${avg.toFixed(1)}`);
 
 
 // Read 6 values that can be floating point numbers. After, print how many of them were positive. In the next line, print the 
