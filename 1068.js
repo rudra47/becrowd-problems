1f1aff1565874b5fde5b96832b8d20d62d08e4ba
RudraSen <rudra.sen@technovista.com.bd>
2023-11-23 16:00:39 +0600

    1068 problem ongoing

---
+++ 1068.js
@@ -0,0 +1,32 @@
+//ODD NUMBER
+var input = `8`;
+var lines = input.split('\n');
+
+let number = Number(lines[0]);
+  
+for (let index = 1; index <= 12; index++) {
+    ++number;
+    let reminder = number % 2;
+    if (reminder !== 0) {
+        console.log(number);
+    }
+}
+
+
+// Read an integer value X and print the 6 consecutive odd numbers from X, a value per line, including X if it is the case.
+
+// Input
+// The input will be a positive integer value.
+
+// Output
+// The output will be a sequence of six odd numbers.
+
+// Input Sample	Output Sample
+// 8
+
+// 9
+// 11
+// 13
+// 15
+// 17
+// 19
