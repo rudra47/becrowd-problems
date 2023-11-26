74c5b2d61345fdfa83661173b8c6ee56d533e89b
RudraSen <rudra.sen@technovista.com.bd>
2023-11-26 10:55:05 +0600

    file rename

--- 1070.js
+++ 1070.js
@@ -1,16 +1,8 @@
-//ODD NUMBER
+//SIX ODD NUMBERS
 var input = `8`;
 var lines = input.split('\n');
 
-let number = Number(lines[0]);
-  
-for (let index = 1; index <= 12; index++) {
-    ++number;
-    let reminder = number % 2;
-    if (reminder !== 0) {
-        console.log(number);
-    }
-}
+
 
 
 // Read an integer value X and print the 6 consecutive odd numbers from X, a value per line, including X if it is the case.
