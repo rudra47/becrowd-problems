51bca8a336398d0e8356b01c45fc6fd5917660b0
RudraSen <rudra.sen@technovista.com.bd>
2023-11-27 10:47:00 +0600

    1071 problem ongoing

---
+++ 1071.js
@@ -0,0 +1,36 @@
+//SIX ODD NUMBERS
+var input = `6
+-5`;
+var lines = input.split('\n');
+
+let number1 = Number(lines[0]);
+let number2 = Number(lines[1]);
+let sum     = 0;
+
+for (let index = number2; index < number1; index++) {
+    let reminder = index % 2;
+    if (reminder !== 0) {
+        sum = sum + index;
+    }   
+}
+
+console.log(sum);
+
+// Read two integer values X and Y. Print the sum of all odd values between them.
+
+// Input
+// The input file contain two integer values.
+
+// Output
+// The program must print an integer number. This number is the sum off all odd values between both input values that must fit in 
+// an integer number.
+
+// Sample Input	Sample Output
+// 6            5
+// -5
+
+// 15           13
+// 12
+
+// 12           0
+// 12
