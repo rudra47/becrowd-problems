5dd99d8037f26207b53db356e3c05f20879fbd27
RudraSen <rudra.sen@technovista.com.bd>
2023-11-28 09:50:55 +0600

    add condition of x

--- 1073.js
+++ 1073.js
@@ -1,12 +1,12 @@
 //INTERVAL 2
-var input = `4
-14
-123
-10
--25`;
+var input = `6`;
 var lines = input.split('\n');
 
 let n = lines[0];
+
+if (n>5 && n<2000) {
+    
+}
 
 
 // Read an integer N. Print the square of each one of the even values from 1 to N including N if it is the case.
@@ -20,9 +20,10 @@ let n = lines[0];
 // Be carefull! Some language automaticly print 1e+006 instead 1000000. Please configure your program to print the correct format 
 // setting the output precision.
 
-// Input Sample	Output Sample
+// Input Sample	
 // 6
 
+//Output Sample
 // 2^2 = 4
 // 4^2 = 16
 // 6^2 = 36
