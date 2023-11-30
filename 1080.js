34d678ec8b10ae649c85d746844dd32089b21dd9
RudraSen <rudra.sen@technovista.com.bd>
2023-11-30 12:00:42 +0600

    1080 problem defined

---
+++ 1080.js
@@ -0,0 +1,29 @@
+//Weighted Average
+var input = `500
+10
+1
+99
+666`; // here will be 100 input. but I have tested with 5
+
+var lines = input.split('\n');
+
+// Read 100 integer numbers. Print the highest read value and the input position.
+
+// Input
+// The input file contains 100 distinct positive integer numbers.
+
+// Output
+// Print the highest number read and the input position of this value, according to the given example.
+
+// Input Sample	
+// 2
+// 113
+// 45
+// 34565
+// 6
+// ...
+// 8
+ 
+//Output Sample
+// 34565
+// 4
