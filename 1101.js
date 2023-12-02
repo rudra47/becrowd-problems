a879ff6b879dcb6a508ab89b8114b2a3e595c5fc
rudra47 <rudrasenr@gmail.com>
2023-12-02 11:48:12 +0600

    1101 problem define

---
+++ 1101.js
@@ -0,0 +1,21 @@
+// Sum of Consecutive Odd Numbers II
+let input = `5 2
+6 3
+5 0`;
+
+let lines = input.split('\n');
+
+
+// Read an undetermined number of pairs values M and N (stop when any of these values is less or equal to zero). For each pair, print the sequence from the smallest to the 
+// biggest (including both) and the sum of consecutive integers between them (including both).
+
+// Input
+// The input file contains pairs of integer values M and N. The last line of the file contains a number zero or negative, or both.
+
+// Output
+// For each pair of numbers, print the sequence from the smallest to the biggest and the sum of these values, as shown below.
+
+// Input Sample	Output Sample
+// 5 2            2 3 4 5 Sum=14
+// 6 3            3 4 5 6 Sum=18
+// 5 0
