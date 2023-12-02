8ab2376a55fdcd415e5a0c77e758d4768f16fc73
rudra47 <rudrasenr@gmail.com>
2023-12-02 11:02:09 +0600

    1098 problem for loop define

---
+++ 1098.js
@@ -0,0 +1,26 @@
+// Sequence IJ 4
+
+for (let i = 0; i <= 2; i += 0.2) {
+   
+}
+
+
+// Make a program that prints the sequence like the following example.
+
+// Input
+// This problem doesn't have input.
+
+// Output
+// Print the sequence like the example below.
+
+//	Output Sample
+// I=0 J=1
+// I=0 J=2
+// I=0 J=3
+// I=0.2 J=1.2
+// I=0.2 J=2.2
+// I=0.2 J=3.2
+// .....
+// I=2 J=?
+// I=2 J=?
+// I=2 J=?
