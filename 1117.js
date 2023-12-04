1afed9f71be8e56a3823c30280f0b7d525a2c319
RudraSen <rudra.sen@technovista.com.bd>
2023-12-04 14:34:11 +0600

    add condition for total score

--- 1117.js
+++ 1117.js
@@ -8,7 +8,13 @@ let lines = input.split('\n');
 let count = 0;
 let totalScore = 0;
 for (let index = 0; index < lines.length; index++) {
-    
+    let score = lines[index];
+    if (score<0 || score>10) {
+        console.log('nota invalida');
+    }else{
+        count++;
+        totalScore += Number(lines[index]);
+    }
 }
 
 // Write a program that reads two scores of a student. Calculate and print the average of these scores. Your program must accept just valid scores [0..10]. Each 
