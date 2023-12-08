87d4859ab367070aa1e818766ef3fa37769f7416
rudra47 <rudrasenr@gmail.com>
2023-12-08 17:19:48 +0600

    variable define

---
+++ 1132.js
@@ -0,0 +1,21 @@
+// Multiples of 13
+let inputs = `100
+200`;
+let lines = inputs.split('\n');
+
+let sum = 0;
+
+
+// Write a program that reads two integer numbers X and Y and calculate the sum of all number not divisible by 13 between them, including both.
+
+// Input
+// The input file contains 2 integer numbers X and Y without any order.
+
+// Output
+// Print the sum of all numbers between X and Y not divisible by 13, including them if it is the case.
+
+// Input Sample	Output Sample
+// 100
+// 200
+
+// 13954
