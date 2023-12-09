cdfdaecbf6bf45a1687002d5827b29a65a10c080
rudra47 <rudrasenr@gmail.com>
2023-12-09 17:02:32 +0600

    loop define

--- 1143.js
+++ 1143.js
@@ -5,7 +5,12 @@ let lines = input.split('\n');
 let n = Number(lines[0]);
 
 if (n > 1 && n < 1000) {
-    
+    for (let i = 1; i <= n; i++) {
+        let a = 1;
+        let output = '';
+        
+        
+    }
 }
 
 // Write a program that reads an integer N (1 < N < 1000). This N is the number of output lines printed by this program.
