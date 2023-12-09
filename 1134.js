d588353adf1ba3ac2b14d1b398952d63aa7936b7
rudra47 <rudrasenr@gmail.com>
2023-12-09 13:03:21 +0600

    1134 problem fix

--- 1134.js
+++ 1134.js
@@ -32,6 +32,7 @@ Gasolina: ${ gasoline}
 Diesel: ${diesel}`);
 
 
+
 // A gas station wants to determine which of their products is the preference of their customers. Write a program to read the type of fuel 
 // supplied (coded as follows: 1. Alcohol 2. Gasoline 3. Diesel 4. End). If you enter an invalid code (outside the range of 1 to 4) a 
 // new code must be requested. The program will end when the inserted code is the number 4.
