a887ea56a2cabc6acbcf26515e4b796a7c3d261d
rudra47 <rudrasenr@gmail.com>
2023-12-09 16:44:33 +0600

    1143 problem define

---
+++ 1143.js
@@ -0,0 +1,20 @@
+// Squared and Cubic
+
+
+// Write a program that reads an integer N (1 < N < 1000). This N is the number of output lines printed by this program.
+
+// Input
+// The input file contains an integer N.
+
+// Output
+// Print the output according to the given example.
+
+// Input Sample	Output Sample
+// 5
+
+// Output Sample
+// 1 1 1
+// 2 4 8
+// 3 9 27
+// 4 16 64
+// 5 25 125
