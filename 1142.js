44fde52b62c8cf83f286e7d2b0766a2be587e646
rudra47 <rudrasenr@gmail.com>
2023-12-09 16:27:06 +0600

    veriable define

---
+++ 1142.js
@@ -0,0 +1,28 @@
+//PUM
+let input = `7`;
+let lines = input.split('\n');
+
+let n = Number(lines[0]);
+
+let count = 1;
+
+
+// Write a program that reads an integer N. This N is the number of output lines printed by this program.
+
+// Input
+// The input file contains an integer N.
+
+// Output
+// Print the output according to the given example.
+
+// Input Sample	Output Sample
+// 7
+
+// Output Sample
+// 1 2 3 PUM
+// 5 6 7 PUM
+// 9 10 11 PUM
+// 13 14 15 PUM
+// 17 18 19 PUM
+// 21 22 23 PUM
+// 25 26 27 PUM
