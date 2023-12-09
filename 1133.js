782a2a5342d7100b021dda2521e8b49313657182
rudra47 <rudrasenr@gmail.com>
2023-12-09 12:11:19 +0600

    1133 problem define

---
+++ 1133.js
@@ -0,0 +1,20 @@
+// Rest of a Division
+let input = `10
+18`;
+let lines = input.split('\n');
+
+//Write a program that reads two integer numbers X and Y. Print all numbers between X and Y which dividing it by 5 the rest is equal to 2 or equal to 3.
+
+// Input
+// The input file contains 2 any positive integers, not necessarily in ascending order.
+
+// Output
+// Print all numbers according to above description, always in ascending order.
+
+// Input Sample	Output Sample
+// 10
+// 18
+
+// 12
+// 13
+// 17
