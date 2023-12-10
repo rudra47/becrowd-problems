17d2afe0265c4e726ce0551543991bffc126f8f0
RudraSen <rudra.sen@technovista.com.bd>
2023-12-10 13:30:38 +0600

    1144 problem solve trying to optimize

--- 1144.js
+++ 1144.js
@@ -38,6 +38,7 @@ for (let i = 1; i <= n; i++) {
 }
 
 
+
 // Write a program that reads an integer N. N * 2 lines must be printed by this program according to the example below. For numbers with more 
 // than 6 digits, all digits must be printed (no cientific notation allowed).
 
