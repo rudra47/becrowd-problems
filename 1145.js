056db03dac01795480e860b7d1808acc9d52baf6
RudraSen <rudra.sen@technovista.com.bd>
2023-12-11 12:22:00 +0600

    initially solve but not enough

--- 1145.js
+++ 1145.js
@@ -5,7 +5,38 @@ let lines = input.split(' ');
 let x = Number(lines[0]);
 let y = Number(lines[1]);
 
+if (x > y) {
+    let b = x;
+    x = y;
+    y = x;
+}
 
+if ((x < y && y < 1000) && (x > 1 && x < 20)) {
+    let digit = 0;
+    let k = y / x;
+    
+    for (let i = 0; i < k; i++) {
+        let output = '';
+        for (let j = 1; j <= x; j++) {
+            digit++;
+            if (j != x) {
+                output += digit + " "
+            }else{
+                output += digit
+            }
+    
+            if (digit === y) {
+                break;
+            }
+        }
+    
+        console.log(output);
+    
+        if (digit === y) {
+            break;
+        }
+    }
+}
 
 
 
