acbeab61f1a14484dd96ecfd369e670dcee2597f
RudraSen <rudra.sen@technovista.com.bd>
2023-12-13 12:09:52 +0600

    input define

--- 1149.js
+++ 1149.js
@@ -1,12 +1,16 @@
 //
-let input = `3 2
-7`;
+let input = `3 -1 0 -2 2`;
 
 
 
 
 
-//Write an algorithm to read a value A and a value N. Print the sum of N numbers from A (inclusive). While N is negative or ZERO, a 
+
+
+
+
+
+// Write an algorithm to read a value A and a value N. Print the sum of N numbers from A (inclusive). While N is negative or ZERO, a 
 // new N (only N) must be read. All input values are in the same line.
 
 // Input
@@ -16,6 +20,5 @@ let input = `3 2
 // The output contains only an integer value.
 
 // Input Sample	Output Sample
-// 3 2             3 -1 0 -2 2
-
-// 7               7
+// 3 2              7
+// 3 -1 0 -2 2      7
