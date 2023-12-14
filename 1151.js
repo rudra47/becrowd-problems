2af3f23e9a29e329c4bd485ebc5a8a85585b89a1
RudraSen <rudra.sen@technovista.com.bd>
2023-12-14 15:04:42 +0600

    1151 fibonacci series ongoing

---
+++ 1151.js
@@ -0,0 +1,50 @@
+//Easy Fibonacci
+let input = `5`;
+let lines = input.split('\n');
+
+let n = Number(lines[0]);
+
+let count = 0;
+let prev1 = 0;
+let prev2 = 0;
+let fibonacci = 0;
+
+function test (num){
+    if (num>n) {
+        return;
+    }
+    process.stdout.write(fibonacci+ " ");
+
+    fibonacci = prev1 + prev2;
+
+    prev1 = prev2;
+    prev2 = count;
+
+    // console.log(prev1, prev2, '~~~~~');
+
+    
+    count++;
+
+    test(count);
+}
+
+test(n);
+
+
+
+// The following sequence of numbers 0 1 1 2 3 5 8 13 21 ... is known as the Fibonacci Sequence. Thereafter, each number after the first 2 is equal to the sum of 
+// the previous two numbers. Write an algorithm that reads an integer N (N < 46) and that print the first N numbers of this sequence.
+
+// Input
+// The input file contains an integer number N (0 < N < 46).
+
+// Output
+// The numbers ​​should be printed on the same line, separated by a blank space. There is no space after the last number.
+
+// Thanks to Cássio F.
+
+// Input Sample	
+// 5
+
+// Output Sample
+// 0 1 1 2 3
