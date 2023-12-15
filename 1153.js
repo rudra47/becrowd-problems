e2c3ad008b5889861d96eafdf6b7c30dd296ff44
rudra47 <rudrasenr@gmail.com>
2023-12-15 22:26:13 +0600

    define loop

--- 1153.js
+++ 1153.js
@@ -5,6 +5,11 @@ let lines = input.split('\n');
 let n = Number(lines[0]);
 let factorial = 1;
 
+for (let i = n; i > 0; i--) {
+    factorial *= i;    
+}
+
+
 // Read a value N. Calculate and write its corresponding factorial. Factorial of N = N * (N-1) * (N-2) * (N-3) * ... * 1.
 
 // Input
