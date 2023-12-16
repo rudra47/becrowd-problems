d4428f6608bcafb962e7ca44a77cfa12b330b838
rudra47 <rudrasenr@gmail.com>
2023-12-16 16:35:11 +0600

    1154 ages problem solve

--- 1154.js
+++ 1154.js
@@ -23,6 +23,7 @@ let average = sum / count;
 
 console.log(average.toFixed(2));
 
+
 // Write an algorithm to read an undeterminated number of data, each containing an individual's age. The final data, which will not enter in the calculations, contains the value of a negative age. Calculate and print the average age of this group of individuals.
 
 // Input
