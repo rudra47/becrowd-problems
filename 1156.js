bb277628a4061f654690403ef7a95b22c876b184
RudraSen <rudra.sen@technovista.com.bd>
2023-12-17 10:18:16 +0600

    1156 problem ongoing 😟

--- 1156.js
+++ 1156.js
@@ -1,6 +1,16 @@
 // S Sequence II
+// S = 1 + 3/2 + 5/4 + 7/8 + ... + 39/?
 
+let result = 0;
+let count = 1;
 
+for (let i = 1; i < 40; i *= 2) {
+    process.stdout.write(`${count}/${i} + `);
+    result += count / i;
+
+    count = count + 2;
+}
+console.log(result.toFixed(2));
 
 
 //Write an algorithm to calculate and write the value of S, S being given by:
