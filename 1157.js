4cdf180a1ebce285c463378c135931e355f93df5
RudraSen <rudra.sen@technovista.com.bd>
2023-12-18 09:18:15 +0600

    1157 define the problem

---
+++ 1157.js
@@ -0,0 +1,18 @@
+//Divisors I
+
+
+// Read an integer N and compute all its divisors.
+
+// Input
+// The input file contains an integer value.
+
+// Output
+// Write all positive divisors of N, one value per line.
+
+// Input Sample	Output Sample
+// 6
+
+// 1
+// 2
+// 3
+// 6
