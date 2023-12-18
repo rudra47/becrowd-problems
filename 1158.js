6a4cf3f962a0f87b697a86f3f88443a6d1138944
RudraSen <rudra.sen@technovista.com.bd>
2023-12-18 10:13:08 +0600

    get the first value and store it in n

--- 1158.js
+++ 1158.js
@@ -9,7 +9,6 @@ let n = Number(lines[0]);
 
 
 
-
 // Read an integer N that is the number of test cases that follows. Each test case contains two integers X and Y. Print one output 
 // line for each test case that is the sum of Y odd numbers from X including it if is the case. For example:
 // for the input 4 5, the output must be 45, that is: 5 + 7 + 9 + 11 + 13
